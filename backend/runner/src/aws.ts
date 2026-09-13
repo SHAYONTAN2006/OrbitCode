49375c5186b88058e54083418eb6b5512b790407
Shayontan2006 <lahashayontan@gmail.com>
2026-09-14 03:35:25 +0530

    explicit credential removal

--- backend/runner/src/aws.ts
+++ backend/runner/src/aws.ts
@@ -3,10 +3,8 @@ import fs from "fs";
 import path from "path";
 
 const s3 = new S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION || "us-east-1"
-})
+});
 
 export const fetchS3Folder = async (
     key: string,
