63588fa1ff5d0e516c8eb0aa7aafb1c9cbbd7d63
Shayontan2006 <lahashayontan@gmail.com>
2026-09-14 03:33:19 +0530

    runner bug

--- backend/runner/src/aws.ts
+++ backend/runner/src/aws.ts
@@ -8,7 +8,10 @@ const s3 = new S3({
     region: process.env.AWS_REGION || "us-east-1"
 })
 
-export const fetchS3Folder = async (key: string, localPath: string): Promise<void> => {
+export const fetchS3Folder = async (
+    key: string,
+    localPath: string
+): Promise<void> => {
     try {
         const params = {
             Bucket: process.env.S3_BUCKET ?? "",
@@ -16,29 +19,40 @@ export const fetchS3Folder = async (key: string, localPath: string): Promise<voi
         };
 
         const response = await s3.listObjectsV2(params).promise();
+
         if (response.Contents) {
-            await Promise.all(response.Contents.map(async (file) => {
-                const fileKey = file.Key;
-                if (fileKey) {
-                    const getObjectParams = {
-                        Bucket: process.env.S3_BUCKET ?? "",
-                        Key: fileKey
-                    };
+            await Promise.all(
+                response.Contents.map(async (file) => {
+                    const fileKey = file.Key;
 
-                    const data = await s3.getObject(getObjectParams).promise();
-                    if (data.Body) {
-                        const fileData = data.Body as Buffer;
-                        const filePath = `${localPath}/${fileKey.replace(key, "")}`;
-                        
-                        await writeFile(filePath, fileData);
+                    // Skip S3 folder/directory markers
+                    if (fileKey && !fileKey.endsWith("/")) {
+                        const getObjectParams = {
+                            Bucket: process.env.S3_BUCKET ?? "",
+                            Key: fileKey
+                        };
 
-                        console.log(`Downloaded ${fileKey} to ${filePath}`);
+                        const data = await s3
+                            .getObject(getObjectParams)
+                            .promise();
+
+                        if (data.Body) {
+                            const fileData = data.Body as Buffer;
+                            const filePath =
+                                `${localPath}/${fileKey.replace(key, "")}`;
+
+                            await writeFile(filePath, fileData);
+
+                            console.log(
+                                `Downloaded ${fileKey} to ${filePath}`
+                            );
+                        }
                     }
-                }
-            }));
+                })
+            );
         }
     } catch (error) {
-        console.error('Error fetching folder:', error);
+        console.error("Error fetching folder:", error);
     }
 };
 
